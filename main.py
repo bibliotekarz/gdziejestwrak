a726e52dfd15b069696a67884600edf438f1dd03
bibliotekarz <arek@bibliotekarz.com>
2021-06-10 10:59:20 +0200

    introduction to file date comparison

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+import datetime
+
 import yagmail
-from os import getenv
+from os import getenv, path
 from dotenv import load_dotenv
+import time
+from datetime import datetime, date
+import requests
+
 
 load_dotenv()
 
 receiver = 'as.po@interia.pl'
-subject = "tytuł ąęółńćźżśą"
-contents = "Nie ma Terrego"
+subject = 'tytuł ąęółńćźżśą'
+contents = 'Nie ma Terrego'
+url_api = 'http://api.sejm.gov.pl/sejm/term9/MP'
 
 
 def send_mail(receiver, subject='hello', contents='...'):
@@ -20,4 +27,26 @@ def send_mail(receiver, subject='hello', contents='...'):
     return
 
 
-send_mail(receiver, subject, contents)
+# send_mail(receiver, subject, contents)
+
+
+def update_data():
+    now = datetime.utcnow().weekday()
+    current_date = datetime.now()
+    modyfi_date = datetime.fromtimestamp(path.getmtime('data.json'))
+    print("\n\n",type(modyfi_date), modyfi_date, " modyfi_date")
+    print(type(current_date), current_date, " current_date")
+    return current_date - modyfi_date
+    # if now == 3 or 6:
+    #     data = requests.get(url_api)
+    #     data.raise_for_status()
+    #     with open("data.json", "w", encoding='utf-8') as f:
+    #         f.write(data.text)
+    # return
+
+
+print(update_data())
+#
+# print(time.ctime(path.getctime('data.json')), ' ctime')
+# print(time.ctime(path.getatime('data.json')), ' atime')
+# print(time.ctime(path.getmtime('data.json')), ' mtime')
