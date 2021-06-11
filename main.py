59521b4389daf9fb3b49e93911b36eb7766e70ee
bibliotekarz <arek@bibliotekarz.com>
2021-06-11 16:24:53 +0200

    test added if file exists

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from datetime import datetime
 import requests
 
+import json
 
 load_dotenv()
 
@@ -28,16 +29,38 @@ def send_mail(receiver, subject='hello', contents='...'):
 
 
 def update_data():
-    now = datetime.now().weekday()
-    current_date = datetime.now().date()
-    modyfi_date = datetime.fromtimestamp(path.getmtime('data.json')).date()
+    status = 'nie pobrano'
+    if path.exists('data.json'):
+        now = datetime.now().weekday()
+        current_date = datetime.now().date()
+        modyfi_date = datetime.fromtimestamp(path.getmtime('data.json')).date()
 
-    if now == (4 or 0) and current_date - modyfi_date == 0:
+        if now == (4 or 0) and current_date - modyfi_date == 0:
+            data = requests.get(url_api)
+            data.raise_for_status()
+            with open("data.json", "w", encoding='utf-8') as f:
+                f.write(data.text)
+            status= 'zaktualizowany'
+    else:
         data = requests.get(url_api)
         data.raise_for_status()
         with open("data.json", "w", encoding='utf-8') as f:
             f.write(data.text)
-    return
+        status = 'pobrany'
+
+    return status
 
 
 # print(update_data())
+
+def work_jason():
+    # Opening JSON file
+    with open('data.json','r', encoding='utf8') as json_file:
+        data = json.load(json_file)
+        print("Type:", type(data), '\n', data)
+
+
+    return
+
+
+work_jason()
