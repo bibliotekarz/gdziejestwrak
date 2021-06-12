3328e52588dcd649b1cf2ef6983d0001ee9cf8aa
bibliotekarz <arek@bibliotekarz.com>
2021-06-12 14:36:35 +0200

    selecting random email from json file

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
+from random import choice
 import yagmail
 from os import getenv, path
 from dotenv import load_dotenv
 from datetime import datetime
 import requests
-
-import json
+from json import load
 
 load_dotenv()
 
@@ -12,6 +12,7 @@ receiver = 'as.po@interia.pl'
 subject = 'tytuł ąęółńćźżśą'
 contents = 'Nie ma Terrego'
 url_api = 'http://api.sejm.gov.pl/sejm/term9/MP'
+club_name = "PiS"
 
 
 def send_mail(receiver, subject='hello', contents='...'):
@@ -29,7 +30,7 @@ def send_mail(receiver, subject='hello', contents='...'):
 
 
 def update_data():
-    status = 'nie pobrano'
+    status = 'database of deputies not downloaded'
     if path.exists('data.json'):
         now = datetime.now().weekday()
         current_date = datetime.now().date()
@@ -40,27 +41,32 @@ def update_data():
             data.raise_for_status()
             with open("data.json", "w", encoding='utf-8') as f:
                 f.write(data.text)
-            status= 'zaktualizowany'
+            status = 'database of deputies has been updated'
     else:
         data = requests.get(url_api)
         data.raise_for_status()
         with open("data.json", "w", encoding='utf-8') as f:
             f.write(data.text)
-        status = 'pobrany'
-
+        status = 'database of deputies downloaded'
     return status
 
 
-# print(update_data())
-
-def work_jason():
-    # Opening JSON file
-    with open('data.json','r', encoding='utf8') as json_file:
-        data = json.load(json_file)
-        print("Type:", type(data), '\n', data)
+print(update_data())
 
 
-    return
+def work_json():
+    i = 0
+    meps_emails = []
+    with open('data.json', 'r', encoding='utf8') as json_file:
+        data = load(json_file)
+        while i < len(data):
+            meps_emails.append(data[i]['email']) if data[i]['club'] == club_name else ""
+            i += 1
+    return meps_emails
 
 
-work_jason()
+def random_email(meps):
+    return choice(meps)
+
+
+print(random_email(work_json()))
